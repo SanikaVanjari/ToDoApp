ea555b1d834b7c3cff8615edd17312b05e10d332
Sanika Vanjari <sanikavanjari94@gmail.com>
2021-09-16 11:46:11 +0530

    back navigation and validation for dialog

--- app/src/main/java/com/practice/todoapp/ToDoDialogFragment.kt
+++ app/src/main/java/com/practice/todoapp/ToDoDialogFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.practice.todoapp.databinding.TodoDialogLayoutBinding
 
-class ToDoDialogFragment : BottomSheetDialogFragment() {
+class ToDoDialogFragment : BottomSheetDialogFragment(), View.OnClickListener {
     private var _binding: TodoDialogLayoutBinding? = null
     private val binding get() = _binding!!
 
@@ -22,11 +24,34 @@ class ToDoDialogFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.saveBt.setOnClickListener(this)
+        binding.closeBt.setOnClickListener(this)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            binding.saveBt -> {
+                if (isToDoNotValid()) {
+                    Toast.makeText(requireContext(), "Please fill all details", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    findNavController().navigateUp()
+                }
+            }
+            binding.closeBt -> {
+                findNavController().navigateUp()
+            }
+            else -> Unit
+        }
+    }
+
+    private fun isToDoNotValid(): Boolean {
+        return binding.titleEt.text.isNullOrEmpty() || binding.descriptionEt.text.isNullOrEmpty()
     }
 
 }
