2ff110411b2f9b9429b6439c3ac34b0ef56dc396
Sanika Vanjari <sanikavanjari94@gmail.com>
2021-09-16 11:14:03 +0530

    Dialog sheet created for add/update todo

--- app/src/main/java/com/practice/todoapp/FirstFragment.kt
+++ app/src/main/java/com/practice/todoapp/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.practice.todoapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.practice.todoapp.databinding.FragmentFirstBinding
 
@@ -32,9 +32,6 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/practice/todoapp/ToDoDialogFragment.kt
@@ -0,0 +1,32 @@
+package com.practice.todoapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.practice.todoapp.databinding.TodoDialogLayoutBinding
+
+class ToDoDialogFragment : BottomSheetDialogFragment() {
+    private var _binding: TodoDialogLayoutBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = TodoDialogLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
